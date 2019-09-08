209b3d9a07d43717851c179d73fb3fc619e0438b
HuaVanAn17051999 <hvan.17it2@sict.udn.vn>
2019-09-08 08:29:07 +0700

    modify

--- HTML/2019-09-03/BTNV-02/script.js
+++ HTML/2019-09-03/BTNV-02/script.js
@@ -5,7 +5,9 @@ function taiDuLieu() {
             var data = JSON.parse(this.responseText);
             var result = data;
             console.log(result);
-            var _count = 0;
+            // result.setAttribute("src", result[0].image_path);
+            // console.log(result[0].image_path);
+            // var _count = 0;
             var img;
             /*Vòng for tạo ra container, do mỗi container có 3 item, nên cần chia làm tròn lên 
             ví dụ:
@@ -13,7 +15,9 @@ function taiDuLieu() {
              vì 3 nhân 3 chỉ được 9 item, cần thêm 1 container để chưa 1 item cho tổng là 10
              10/3=3.3333 => làm tròn lên 3.3333 = 4
             */
-            for (var i = 0; i < (result.length / 3); i++) {
+            var doDaiArray = result.length / 3;
+            console.log(doDaiArray);
+            for (var i = 0; i < doDaiArray; i++) {
                 //Cần code jquery tạo ra từng container sau đó lặp container đó
                 var divContainer = document.createElement('div');
                 divContainer.setAttribute('class', 'container');
@@ -22,20 +26,44 @@ function taiDuLieu() {
                 divRow.setAttribute('class', 'row');
                 //Cần code jquery tạo ra từng dòng(col) sau đó lặp dòng() đó
                 for (var j = 0; j < 3; j++) {
-                    var divColumn = document.createElement('div');
-                    divColumn.setAttribute('class', 'card col-4');
-                    var divCard = document.createElement('div');
-                    var cardBody = document.createElement('div');
-                    cardBody.setAttribute('class', 'card-body');
 
-                    // var img = document.createElement('img');
-                    // img.setAttribute("src", (result[i * 3 + j]).image_url);
-                    // img.setAttribute('class', 'card-img-top')
-                    // divColumn.appendChild(img);
+                    if (i == 3 && j >= 1) {
 
-                    divColumn.appendChild(cardBody);
-                    divColumn.appendChild(divCard);
-                    divRow.appendChild(divColumn);
+                    } else {
+                        console.log(i, j);
+                        var divColumn = document.createElement('div');
+                        divColumn.setAttribute('class', 'card col-4');
+                        var divCard = document.createElement('div');
+                        var cardBody = document.createElement('div');
+                        cardBody.setAttribute('class', 'card-body');
+
+                        var img = document.createElement('img');
+                        // console.log(i * 3 + j);
+                        img.setAttribute("src", (result[i * 3 + j]).image_url);
+                        img.setAttribute('class', 'card-img-top')
+                        divColumn.appendChild(img);
+
+                        var h5 = document.createElement('h5');
+                        h5.innerHTML = result[i * 3 + j].title;
+                        h5.setAttribute('class', 'card-title')
+                        cardBody.appendChild(h5);
+
+                        var p = document.createElement('p');
+                        p.innerHTML = result[i * 3 + j].description;
+                        cardBody.appendChild(p);
+                        p.setAttribute('class', 'card-text')
+
+                        var divContent = document.createElement('div');
+                        divContent.innerHTML = result[i * 3 + j].content;
+                        cardBody.appendChild(divContent);
+                        // console.log(divContent);
+
+
+
+                        divColumn.appendChild(cardBody);
+                        divColumn.appendChild(divCard);
+                        divRow.appendChild(divColumn);
+                    }
                     // _count++;
                     //Đến số lượng bản ghi đã được tạo ra xem đã lớn hơn số bản ghi json trả về chưa, nếu lớn hơn thì không tạo row col-4 nữa
                     // if (_count === (result.length - 1)) {
